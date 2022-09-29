ef511c21a6f992566242aea07b3f9e3d4936a90e
Radamir19 <92337596+Radamir19@users.noreply.github.com>
2022-09-29 08:53:40 +0300

    update hw.py

--- hw.py
+++ hw.py
@@ -4,13 +4,20 @@ max = 0
 min = 10 ** 9
 for i in range(2, n):
     if n % i == 0:
-        a.append(i)
+        b = []
+        for j in range(2,i):
+            if i % j == 0:
+                b.append(j)
+        if len(b) == 0:
+            a.append(i)
 for i in range(len(a)):
     if a[i] > max:
-        max = i
+        max = a[i]
+        i_max = i
     if a[i] < min:
-        min = i
+        min = a[i]
+        i_max = i
 print(*a)
-a[min], a[max] = a[max], a[min]
+a[i_min], a[i_max] = a[i_max], a[i_min]
 print(*a)
 
